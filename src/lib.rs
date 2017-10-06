1cd832abef35b5bc64a101ad5c11faff69072057
Federico Mena Quintero <federico@gnome.org>
2017-10-06 15:53:18 -0500

    Make the ASCII art more legible by not using escapes

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,14 @@ use std::iter::repeat;
 // Constants! :D
 const ENDSL:   &[u8] = b"| ";
 const ENDSR:   &[u8] = b" |\n";
-const FERRIS:  &[u8] = b"
-              \\
-               \\
+const FERRIS:  &[u8] = br#"
+              \
+               \
                   _~^~^~_
-              \\) /  o o  \\ (/
+              \) /  o o  \ (/
                 '_   -   _'
-                / '-----' \\
-";
+                / '-----' \
+"#;
 const NEWLINE: u8 = '\n' as u8;
 const SPACE:   u8  = ' ' as u8;
 const DASH:    u8  = '-' as u8;
